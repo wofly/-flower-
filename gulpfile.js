f5b9f13220c0808a47d1358fac4c887f063ef3b5
wofly <13092945295@163.com>
2019-03-11 23:22:36 +0800

    11号晚上

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
 let cssnano = require('gulp-cssnano');
+let cleanscc = require('gulp-clean-css');
 gulp.task('css',function(){
 	gulp.src('./sass/flower.scss')
 	.pipe(sass())
+	//.pipe(cssnano())
 	.pipe(gulp.dest('./css'))
-})
+});
 gulp.task('aa',function(){
 	gulp.watch(['./sass/flower.scss'],['css'])
 })
